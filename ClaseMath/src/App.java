19410202a08013bb0aa0bc9fe61546661ac9e1d6
ClaudiaMoraC <claudhiiiamora@gmail.com>
2021-07-01 12:04:45 -0500

    Clase matematica

---
+++ ClaseMath/src/App.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+import java.math.*;
+public class App {
+    public static void main(String[] args) throws Exception {
+        double raiz = Math.sqrt(3);//Raiz cuadrada
+        System.out.println(raiz);
+        double resultado = Math.pow(4, 2);//Raiz cuadrada (base, exponente)
+        System.out.println(resultado);
+    }
+}
